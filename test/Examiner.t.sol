b4944036eb20dffdca62f27f46d5cbb10c823fa3
Senul <hardlove.mrplus@gmail.com>
2022-10-23 19:58:35 +0530

    test contract edited

--- test/Examiner.t.sol
+++ test/Examiner.t.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "forge-std/console.sol";
 import "../contracts/Examiner.sol";
 
 contract ExaminerTest is Test {
@@ -20,5 +21,26 @@ contract ExaminerTest is Test {
     function testAuthenticity() external {
       vm.prank(realCaller);
       examiner.changeOrg(realCaller);
+    }
+
+    function testFeeCalcutation(uint256 amount) external {
+      vm.assume(amount > 10000);
+      uint factor = 100;
+      vm.deal(address(this), amount);
+      uint256 fee = amount / factor;
+      if(fee == 0){
+          revert();
+        }
+      uint256 payableAmount = amount - fee;
+
+      transferNativeFunds(payableAmount, payable(realCaller));
+      uint bal = address(this).balance;
+      assertEq(fee, bal);
+    }
+
+     // Simple functions to avoid code repetition to transfer native currency
+    function transferNativeFunds(uint256 amount, address payee) internal {
+        (bool success, ) = payable(payee).call{value: amount}("");
+        require(success, "Transfer Failed");
     }
 }
