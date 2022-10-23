82a9522273135cf9f7b75e77b83a0b1f692062bb
Senul <hardlove.mrplus@gmail.com>
2022-10-23 18:26:12 +0530

    minor changes

--- test/Examiner.t.sol
+++ test/Examiner.t.sol
@@ -6,12 +6,19 @@ import "../contracts/Examiner.sol";
 
 contract ExaminerTest is Test {
     Examiner public examiner;
+
+    address realCaller = 0x15Cf49f835C5545810a2CCd4c8F71B17dc16aE22;
+
     function setUp() public {
-       examiner = new Examiner(2, msg.sender);
+       examiner = new Examiner(2, realCaller);
     }
 
-    function testOwner() external {
-        address organ = examiner.org();
-        assertEq(organ, msg.sender);
+    function testOwnership() external {
+      assertEq(realCaller, examiner.org());
+    }
+
+    function testAuthenticity() external {
+      vm.prank(realCaller);
+      examiner.changeOrg(realCaller);
     }
 }
