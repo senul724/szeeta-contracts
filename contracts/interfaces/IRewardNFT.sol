b8d03597ae156db3d9c525d691c3a2371a418c6a
Senul <hardlove.mrplus@gmail.com>
2022-11-11 21:18:43 +0530

    mint function included

--- contracts/interfaces/IRewardNFT.sol
+++ contracts/interfaces/IRewardNFT.sol
@@ -3,5 +3,6 @@ pragma solidity ^0.8.16;
 
 interface IRewardNFT{
     function close() external;
+    function mint(address contributor) external returns(uint256);
     function transferOwnership(address newOwner) external;
 }
