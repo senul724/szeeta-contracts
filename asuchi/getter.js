3cf9e367fb60ad58050c9a41157f673d66b7022d
Senul <hardlove.mrplus@gmail.com>
2022-11-14 19:12:08 +0530

    script removed

--- asuchi/getter.js
+++
@@ -1,5 +0,0 @@
-const {Interface, FormatTypes} = require("@ethersproject/abi")
-const abi = require('../build/NetworkAdmin.sol/NetworkAdmin.json').abi
-
-const out = new Interface(abi).format(FormatTypes.full);
-console.log(out)
