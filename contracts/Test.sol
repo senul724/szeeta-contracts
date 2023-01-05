f11f219d40e4714529959050bd86fd6af02bebbe
Senul <hardlove.mrplus@gmail.com>
2023-01-05 13:21:03 +0530

    test contract modified

--- contracts/Test.sol
+++ contracts/Test.sol
@@ -2,7 +2,13 @@
 pragma solidity ^0.8.16;
 
 contract Test{
+  address public owner;
+
+  constructor(address owner_) {
+    owner = owner_;
+  }
+
   function getTime() external view returns(uint){
       return block.timestamp;
-    }
+  }
 }
