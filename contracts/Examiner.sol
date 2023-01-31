6c2775a01e48bbda016e503d359f6e68f74e9350
Senul <hardlove.mrplus@gmail.com>
2023-01-31 14:59:52 +0530

    typos fixed

--- contracts/Examiner.sol
+++ contracts/Examiner.sol
@@ -6,20 +6,20 @@ import "./openzeppalin-utils/SafeERC20.sol";
 import "./openzeppalin-utils/EIP712.sol";
 
 /**
- * @dev Examiner is a contract which facilitate effective contribution collection
+ * @dev Examiner is a contract that facilitates effective contribution collection
  * with blockchain tech.
  *
- * Functionality of the contract is to recieve contributions, validate the transactions,
- * deduct the fee and transfer the funds to the specific reciever.
+ * Functionality of the contract is to receive contributions, validate the transactions,
+ * deduct the fee and transfer the funds to the specific receiver.
  *
  * The contributions are sent to an event's receiving address where the event id and the 
- * receiving address are sent as arguments. Also the transaction data is sucured and 
- * validated using EIP712 and used nonces to avoid replaying and re-entrancy.
+ * receiving addresses are sent as arguments. Also, the transaction data is secured and 
+ * validated using EIP712 and used nonces to avoid replaying and re-entrance.
  *
- * Contributions are accepted through native currency of the network and ERC20 tokens
+ * Contributions are accepted through the native currency of the network and ERC20 tokens
  * allowed by the organization.
  *
- * Governer address is responsible for changing sensitive information realted to the organization
+ * Governor address is responsible for changing sensitive information related to the organization
  * and withdrawing fees.
  */
 contract Examiner is EIP712{
@@ -36,17 +36,17 @@ contract Examiner is EIP712{
     /**
      * @dev Address of the organization.
      */
-    address private governer;
+    address private governor;
 
     /**
      * @dev Method used to calculate the fee
      *
-     * When the amount is devided by the fee factor, the quotient will be the fee.
-     * example: If the fee is 1%, feeFactor will be 100.
+     * When the amount is divided by the fee factor, the quotient will be the fee.
+     * Example: If the fee is 1%, feeFactor will be 100.
      */
     uint256 public feeFactor;
 
-    // Keeps in track of the processed transaction count
+    // Keeps track of the processed transaction count
     uint256 private transactionCount;
 
     // Structs
@@ -107,33 +107,33 @@ contract Examiner is EIP712{
     );
 
     // modifiers
-    modifier onlyGoverner(){
-        require(msg.sender == governer, "Unauthorized Call!");
+    modifier onlyGovernor(){
+        require(msg.sender == governor, "Unauthorized Call!");
         _;
     }
 
-    constructor(uint fee, address org_, address governer_) EIP712('szeeta', '0.0.1'){
+    constructor(uint fee, address org_, address governor_) EIP712('szeeta', '0.0.1'){
         feeFactor = fee;
         org = org_;
-        governer = governer_;
+        governor = governor_;
         transactionCount = 1;
     }
 
     // public functions
 
     /**
-     * @dev About the recieve funcitions
+     * @dev About the receive functions
      *
-     * The receive functions are main functional components of the contract.
+     * The receive functions are the main functional components of the contract.
      * 
-     * These functions receive contributions, validates the transaction(process is metioned below)
-     * and split the amount sending the amount to the receiver deducting the fee. 
+     * These functions receive contributions, validates the transaction(process is mentioned below)
+     * and split the amount sending the amount to the receiver and deducting the fee. 
      *
-     * For native currency contributions, the fee is collected in contract for organization to
-     * withdraw and for ERC20 contributions the fee is transferd to the organization immediatly.
+     * For native currency contributions, the fee is collected in the contract for the organization to
+     * withdraw and for ERC20 contributions the fee is transferred to the organization immediately.
      *
-     * About ERC20 tokens, a list of popular tokena are seleceted by the organization and allowed.
-     * Also we allow users to suggest token to allow.
+     * About ERC20 tokens, a list of popular tokens are selected by the organization and allowed.
+     * Also we allow users to suggest tokens to allow.
      */
     
     /**
@@ -154,7 +154,7 @@ contract Examiner is EIP712{
         require(
             validataSignatureForTokens(eventId, amount, amountInUsd, token, receiver, nonce, signature)
         );
-        //incrementing the transaction count to mark as processed
+        //incrementing the transaction count to mark it as processed
         transactionCount = nonce + 1;
         // calculating the fee
         uint256 fee = amount / feeFactor;
@@ -188,7 +188,7 @@ contract Examiner is EIP712{
             msg.value == amount &&
             validateSignature(eventId, amount, amountInUsd, receiver, nonce, signature)
         );
-        //incrementing the transaction count to mark as processed
+        //incrementing the transaction count to mark it as processed
         transactionCount = nonce + 1;
         uint256 fee = amount / feeFactor;
         transferNativeFunds(amount - fee, receiver);
@@ -204,7 +204,7 @@ contract Examiner is EIP712{
     // internal
 
     /**
-     * @dev Simple function for transfering native contributions.
+     * @dev Simple function for transferring native contributions.
      * Mainly done to avoid code repetition.
      */
     function transferNativeFunds(uint256 amount, address payee) internal {
@@ -230,8 +230,8 @@ contract Examiner is EIP712{
     /**
      * @dev About the transactions validations
      *
-     * We use transaction count as a nonce to avoid re-entrancy or replaying.
-     * And a organizational signed EIP712 signature is used to avoid thirdparty method calls.
+     * We use transaction count as a nonce to avoid re-entrance or replaying.
+     * And organizational signed EIP712 signature is used to avoid third-party method calls.
      */
 
     /**
@@ -286,27 +286,27 @@ contract Examiner is EIP712{
     // For organizational use
 
     /**
-     * @dev All contributions are sent out of the contract immediatly so that only the fee
+     * @dev All contributions are sent out of the contract immediately so that only the fee
      * is collected in the contracts.
      *
-     * Value to withdraw is passed as and argument and not setting the amount to be contract balance is to avoid
+     * Value to withdraw is passed as an argument and not setting the amount to contract balance is to avoid
      * any errors due to order of execution 
      */
-    function drain(uint amount) external onlyGoverner{
-        transferNativeFunds(amount, governer);
+    function drain(uint amount) external onlyGovernor{
+        transferNativeFunds(amount, governor);
     }
 
     /**
      * @dev restricted function to change organization address
      */
-    function changeOrg(address newOrg) external onlyGoverner{
+    function changeOrg(address newOrg) external onlyGovernor{
         org = newOrg;
     }
 
     /**
-     * @dev restricted function to change governer address
+     * @dev restricted function to change governor address
      */
-    function changeGoverner(address newGoverner) external onlyGoverner{
-        governer = newGoverner;
+    function changeGovernor(address newGovernor) external onlyGovernor{
+        governor = newGovernor;
     }
 }
