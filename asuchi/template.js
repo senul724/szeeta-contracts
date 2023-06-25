06b31aea93ba1d270f23d563a7089813bff3ebb5
Senul <hardlove.mrplus@gmail.com>
2023-06-25 21:03:44 +0530

    new template added

--- asuchi/template.js
+++ asuchi/template.js
@@ -1,7 +1,22 @@
 const ethers = require('ethers');
 
-const mumbai_rpc = "https://holy-boldest-sun.bsc.discover.quiknode.pro/51ae2de80450e4bcf458bbfe7daf24277d75b242/"
+const rpc = ""
+const abi = [""]
+const addr = ""
+const pvt_key = ""
 
 // derived
-const provider = new ethers.providers.JsonRpcProvider(mumbai_rpc)
-provider.getNetwork().then(console.log)
+const provider = new ethers.providers.JsonRpcProvider(rpc)
+  const wallet = new ethers.Wallet(pvt_key).connect(
+    provider,
+  );
+const contract = new ethers.Contract(addr, abi, wallet)
+
+const run = async()=>{
+  const res = await contract.get()
+  console.log("sent")
+  await res.wait();
+  console.log("minted")
+
+}
+run();
